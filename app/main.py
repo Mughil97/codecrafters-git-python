3be0037d2dd2ce1b0af732a519142c129f42db13
Mughil97 <mmughil97@gmail.com>
2024-11-04 13:55:36 +0530

    second stage

--- app/main.py
+++ app/main.py
@@ -1,22 +1,38 @@
-import sys
+import argparse
 import os
+import sys
+import zlib
 
 
 def main():
-    # You can use print statements as follows for debugging, they'll be visible when running tests.
-    print("Logs from your program will appear here!")
 
+    arg_parser = argparse.ArgumentParser()
+    arg_parser.add_argument("command")
+    arg_parser.add_argument("-p", dest="blob_path")
+    args = arg_parser.parse_args()
 
-    command = sys.argv[1]
+    command = args.command
     if command == "init":
-        os.mkdir(".git")
-        os.mkdir(".git/objects")
-        os.mkdir(".git/refs")
-        with open(".git/HEAD", "w") as f:
-            f.write("ref: refs/heads/main\n")
-        print("Initialized git directory")
+        init()
+    elif command == "cat-file":
+        cat_file(args.blob_path)
     else:
         raise RuntimeError(f"Unknown command #{command}")
+
+
+def cat_file(blob_path):
+    with open(rf".git/objects/{blob_path[:2]}/{blob_path[2:]}", "rb") as f:
+        # f.readlines()
+        print(zlib.decompress(f.read()).decode("utf-8").split("\0")[1], end="")
+
+
+def init():
+    os.mkdir(".git")
+    os.mkdir(".git/objects")
+    os.mkdir(".git/refs")
+    with open(".git/HEAD", "w") as f:
+        f.write("ref: refs/heads/main\n")
+    print("Initialized git directory")
 
 
 if __name__ == "__main__":
